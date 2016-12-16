bf81ed90bd9a983adc1dc3467be096418921bbf1
Heng Li <lh3@me.com>
2016-12-16 15:20:24 -0500

    kt_for with a mini thread pool; buggy
    deadlock on mac sometimes

--- kthread.h
+++ kthread.h
@@ -8,6 +8,10 @@ extern "C" {
 void kt_for(int n_threads, void (*func)(void*,long,int), void *data, long n);
 void kt_pipeline(int n_threads, void *(*func)(void*, int, void*), void *shared_data, int n_steps);
 
+void *kt_forpool_init(int n_threads);
+void kt_forpool_destroy(void *_fp);
+void kt_forpool(void *_fp, void (*func)(void*,long,int), void *data, long n);
+
 #ifdef __cplusplus
 }
 #endif
