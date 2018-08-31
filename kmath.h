fd1bf6d3f043308299f914617a6f3008ffd9b08d
Heng Li <lh3@me.com>
2018-08-30 22:58:52 -1000

    removed MT19937-64.

--- kmath.h
+++ kmath.h
@@ -7,22 +7,6 @@
 extern "C" {
 #endif
 
-	/**********************************
-	 * Pseudo-random number generator *
-	 **********************************/
-
-	typedef uint64_t krint64_t;
-
-	struct _krand_t;
-	typedef struct _krand_t krand_t;
-
-	#define kr_drand(_kr) ((kr_rand(_kr) >> 11) * (1.0/9007199254740992.0))
-	#define kr_sample(_kr, _k, _cnt) ((*(_cnt))++ < (_k)? *(_cnt) - 1 : kr_rand(_kr) % *(_cnt))
-
-	krand_t *kr_srand(krint64_t seed);
-	krint64_t kr_rand(krand_t *kr);
-	double kr_normal(krand_t *kr);
-
 	/**************************
 	 * Non-linear programming *
 	 **************************/
